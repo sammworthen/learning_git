d0eededba6507080a731f067ec5ef5c6a247b2f9
Sammie Worthen <sammworthen@gmail.com>
2017-04-20 20:30:33 -0600

    age and if else

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ console.log('We are on a new branch!');
 
 var firstName = 'Sammie';
 var lastName = 'Worthen';
+var age = 28;
 
 console.log('Full Name: ' + firstName + ' ' + lastName);
+
+if(age > 19)
+  console.loc('Not a teen');
+else
+  console.log('we are a teen. We hate our parents');
