ef02abbdb50c7d5c232c3ef968e338176e43acb6
Sammie Worthen <sammworthen@gmail.com>
2017-04-20 20:21:57 -0600

    console logs in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Welcome to Git!');
+
+console.log('We are on a new branch!');
+
+var firstName = 'Sammie';
+var lastName = 'Worthen';
+
+console.log('Full Name: ' + firstName + ' ' + lastName);
